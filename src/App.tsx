a483db5179fa84c751f81b50439620339f7a0e1e
LaurentKatsesSandalman <laurent.durup@numericable.fr>
2025-04-14 11:15:33 +0200

    react side

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import './App.css'
+import EmployeeCard from './components/EmployeeCard'
+
+const sampleEmployee = {
+  name: {
+    first: "Claire",
+    last: "Theson",
+  },
+  email: "claire.theson@example.com",
+  picture: {
+    medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
+  },
+};
+
+function App() {
+  const [employee, setEmployee] = useState(sampleEmployee);
+
+  const getEmployee = () => {
+    // Send the request
+    fetch("http://localhost:3310/api/employees")
+      .then((response) => response.json())
+      .then((data) => {
+        setEmployee(data.results[0]);
+      });
+  };
+
+  return (
+    <div className='App'>
+      <EmployeeCard employee={employee} />
+      <button type="button" onClick={getEmployee}>Get employee</button>
+    </div>
+  );
+}
+
+export default App
